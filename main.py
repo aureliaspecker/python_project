25c2e918ef02ad1d4c5c52b38d420baec882d134
aureliaspecker <aureliaspecker@users.noreply.github.com>
2018-04-11 20:25:48 +0100

    Adding @app.route(events)

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask("Whatweatherwhere")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
+
+    endpoint = "http://api.openweathermap.org/data/2.5/weather"
+
+    print(request)
     return render_template("main.html")
+
+@app.route("/events", methods=['GET', 'POST'])
+def events():
+    print(request.form['location'])
+
+        playload = {}
+
+#       { data['location'] : 'London',
+#       data['keyword'] : 'London',
+
+    return render_template("main.html", data)
 
 app.run(debug=True)
