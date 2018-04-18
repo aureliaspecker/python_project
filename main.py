28efb111d502890c5a17286046a71861cfde7d61
Himalee <himalee.tailor@gmail.com>
2018-04-18 13:45:06 +0100

    App will tweet upon user search

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from flask import Flask, render_template, request
 
 app = Flask("Whatweatherwhere")
 
-port = int(os.environ.get("PORT", 5000)) # saved the key as an environment variable
+# saved the weather API key as an environment variable
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     print port #what is this doing?
-    return render_template("main.html")
+    return render_template("main.html") # renders the main page
+
+# Calling the API using the endpoint and secret key.
+# Defining a "pay load" which is sent to the API in order for the user to be able to see results based on city name.
 
 @app.route("/weather", methods=['GET', 'POST'])
 def events():
@@ -24,6 +28,8 @@ def events():
     }
     response = requests.get(endpoint, params=payload)
     print response.url
+    # "weather" is a dictionary - the responses are the pieces of data we want to use and display within a list the API creates.
+    # response.json converts the API string into a list.
     weather = {}
     weather["main"] = response.json()["weather"][0]["main"]
     weather["temp"] = response.json()["main"]["temp"]
@@ -47,6 +53,25 @@ def events():
     #
     #
     # return render_template("events.html", city_weather=weather, city_wear=wear)
+
+    api = twitter.Api(
+        consumer_key = os.environ.get("consumer_key"),
+        consumer_secret = os.environ.get("consumer_secret"),
+        access_token_key = os.environ.get("access_token_key"),
+        access_token_secret = os.environ.get("access_token_secret")
+    )
+
+    print(api.VerifyCredentials())
+    {"id": 986265829162483712, "location": "London", "name": "WhatWeatherWear"}
+
+    # AUTOMATIC TWEET WHEN USER SEARCHES
+    status = api.PostUpdate("The temperature in {} is {} degrees. #{} #whatweatherwhere".format(weather["name"],weather["temp"],weather["main"]))
+    app.logger.info(status.text)
+
+    # List of if statements based on "main" data from the open weather API.
+    # Renders weather.html and assigns a css class so we only need to create one css file.
+    # We can call the API data and weather tips in our html file using jinja syntax (city_weather and city_wear).
+    # Added an else statement if a "main" weather type has not been defined in our if statements.
 
     if weather["main"] == "Rain":
         rain_wear = "Take a raincoat and an umbrella!"
@@ -79,19 +104,10 @@ def events():
         return render_template("weather.html", css_class="default", city_weather=weather)
 
 
-    api = twitter.Api(twitter_consumer_key = os.environ.get("twitter_consumer_key"),
-                      twitter_consumer_secret = os.environ.get("twitter_consumer_secret"),
-                      twitter_access_token_key = os.environ.get("twitter_access_token_key"),
-                      twitter_access_token_secret = os.environ.get("twitter_access_token_key"))
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 
 
-    print(api.VerifyCredentials())
-    {"id": 986265829162483712, "location": "London", "name": "WhatWeatherWear"}
-
-    # AUTOMATIC TWEET WHEN USER SEARCHES
-    status = api.PostUpdate("Hello")
-    print(status.text)
-    Hello
-
-
-app.run(host='0.0.0.0', port=port, debug=True) # making sure application will start on the specific port
+# Making sure application will start on the specific port
+app.run(host='0.0.0.0', port=port, debug=True)
--- static/test.py
+++ static/test.py
@@ -1,16 +1,3 @@
-import twitter
-
-api = twitter.Api(consumer_key='nZaBDWJQJ4V2iBfjkosROsBS7',
-                      consumer_secret='XGzOKAebDPZeEP96zPbqExmzYghpxVmpRTBzaSvrnXZzafy6c9',
-                      access_token_key='986265829162483712-Q4YqfeGHoJDbp97IiIHZen2Ib6Qhgz0',
-                      access_token_secret='DTGVmQWjzEvgq6A8902UPJmYiN1MLZA7qN8HWcwF9bYzI')
-
-print(api.VerifyCredentials())
-{"id": 986265829162483712, "location": "London", "name": "WhatWeatherWear"}
-
-# TO TWEET - ADD TEXT
-status = api.PostUpdate('What the weather is, where you are, and what to wear! #WhatWeatherWear')
-print(status.text)
 
 
 # TO GET TWEETS FROM USER (in this case What Weather Wear)
@@ -28,4 +15,4 @@ print(status.text)
 #        w.pack()
 
 
-#numberOfTweets = 5        
+#numberOfTweets = 5
